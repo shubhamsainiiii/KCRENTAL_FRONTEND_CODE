b1815ca65a5424413b65f88e516b664389864079
Shubham Saini <shubhamthoi27@gmail.com>
2026-01-23 22:11:15 +0530

    new changes

--- src/Admin/JewelryList.jsx
+++ src/Admin/JewelryList.jsx
@@ -433,7 +433,7 @@ const JewelryList = () => {
                     </div>
 
 
-                    <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
+                    <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-8">
                         {filteredJewelry.map((item) => (
                             <div
                                 key={item._id}
